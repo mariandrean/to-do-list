44a8f0af5cb614105acd71272d785e7fa802b007
mariandrean <m.andreaan@gmail.com>
2024-02-13 10:04:22 +0100

    add fetch

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 import './Home.css';
 import Button from '../../components/Button/Button'
+import { useEffect, useState } from 'react';
+
 
 function Home() {
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/todos")
+        .then(response => response.json())
+        .then(list => setTodos(list));
+    },[]);
+
     return(
         <>
-            <h1>Home</h1>
+            <h1>Mi lista de tareas</h1>
+            <section>{todos.map(todo => (<p>{todo.name}</p>))}</section>
             <Button/>
         </>
     )
