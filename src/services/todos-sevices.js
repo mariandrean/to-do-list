f5444269bde3648101e40ca7c3883e6bd2cad0e7
mariandrean <m.andreaan@gmail.com>
2024-02-27 09:20:55 +0100

    chaging fetch to axios in todos-services.js

--- src/services/todos-sevices.js
+++ src/services/todos-sevices.js
@@ -1,4 +1,53 @@
+// PETICIONES CON AXIOS
+
+import axios from "axios";
+
 // Petición GET
+
+export const getToDoList = async () => {
+    const response = await axios.get('http://localhost:8000/todos');
+    const data = await response.data;
+    return data;
+}
+
+export const getTaskById = async (id) => {
+    const response = await axios.get(`http://localhost:8000/todos/${id}`);
+    const data = await response.data;
+    return data;
+}
+
+// Petición DELETE
+
+export const deleteTask = async (id) => {
+    if (confirm("¿Estás seguro que quieres eliminar?") === true) {
+        const response = await axios.delete(`http://localhost:8000/todos/${id}`, { method: "DELETE" });
+        const data = await response.data;
+        alert(`Tarea eliminada: ${data.name}`);
+        return data;
+    }
+}
+
+// Petición POST
+
+export const createTask = async (task) => {
+    const response = await axios.post('http://localhost:8000/todos', task);
+    const data = await response.data;
+    alert(`Tarea añadida con éxito: ${data.name}`);    
+    return data;
+}
+
+// Petición PUT
+
+export const updateTask = async (id, task) => {
+    const response = await axios.put(`http://localhost:8000/todos/${id}`, task);
+    const data = await response.data;
+    alert(`Tarea actualizada: ${data.name}`);
+    return data;
+}
+
+// PETICIONES CON FETCH
+
+/* // Petición GET
 
 export const getToDoList = async () => {
     const response = await fetch('http://localhost:8000/todos');
@@ -48,4 +97,4 @@ export const updateTask = async (id, modifiedTask) => {
     const data = await response.json();
     alert(`Tarea actualizada: ${modifiedTask.name}`);
     return data;
-}
+} */
