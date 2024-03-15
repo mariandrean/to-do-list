911d2f323150e85fad4ae2441503ef023f9d69de
mariandrean <m.andreaan@gmail.com>
2024-03-15 09:53:20 +0100

    modifying validators

--- backend/routes/TaskRouter.js
+++ backend/routes/TaskRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { createTask, deleteTask, getOneTask, getTasks, updateTask } from "../controllers/TaskController.js"
-import { CreateTaskValidator, UpdateTaskValidator, DeleteTaskValidator } from "../validators/TaskValidator.js";
+import { validateCreateTask, validateTaskExistence, validateUpdateTask } from "../validators/TaskValidator.js";
 
 const TaskRouter = express.Router();
 
 TaskRouter.get("/", getTasks);
 TaskRouter.get("/:id", getOneTask);
-TaskRouter.post("/", CreateTaskValidator, createTask);
-TaskRouter.put("/:id", UpdateTaskValidator, updateTask);
-TaskRouter.delete("/:id", DeleteTaskValidator, deleteTask)
+TaskRouter.post("/", validateCreateTask, createTask);
+TaskRouter.put("/:id", validateTaskExistence, validateUpdateTask, updateTask);
+TaskRouter.delete("/:id", validateTaskExistence, deleteTask)
 
 export default TaskRouter;
--- backend/validators/TaskValidator.js
+++ backend/validators/TaskValidator.js
@@ -2,7 +2,7 @@ import { body, check, param } from "express-validator";
 import { validateResult } from "../helpers/validateHelpers.js";
 import TaskModel from "../models/TaskModel.js";
 
-export const CreateTaskValidator = [
+export const validateCreateTask = [
     check("name")
         .notEmpty().withMessage("name cannot be null")
         .custom(async name => {
@@ -17,29 +17,12 @@ export const CreateTaskValidator = [
     }
 ];
 
-export const UpdateTaskValidator = [
-    check("name")
-        .notEmpty().withMessage("name cannot be null"),
-
+export const validateTaskExistence = [
     param("id")
         .custom(async id => {
             const task = await TaskModel.findByPk(id);
             if (!task) {
-                throw new Error('does not exist');
-            }
-        }),
-
-    (req, res, next) => {
-        validateResult(req, res, next)
-    }
-];
-
-export const DeleteTaskValidator = [
-    param("id")
-        .custom(async id => {
-            const task = await TaskModel.findByPk(id);
-            if (!task) {
-                throw new Error('does not exist');
+                throw new Error(`The task with id:${id} does not exist`);
             }
         }),
 
@@ -47,3 +30,11 @@ export const DeleteTaskValidator = [
         validateResult(req, res, next)
     }
 ]
+
+export const validateUpdateTask = [
+    check("name")
+        .notEmpty().withMessage("name cannot be null"),
+    (req, res, next) => {
+        validateResult(req, res, next)
+    }
+];
