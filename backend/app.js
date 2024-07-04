d08e2e18fc9140af0a5adb8db44ef589be6cc3fa
mariandrean <m.andreaan@gmail.com>
2024-07-04 18:02:20 +0200

    move model sync to app.js

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,20 @@ import TaskRouter from "./routes/TaskRouter.js";
 import { NODE_ENV, PORT } from "./config.js";
 import chalk from "chalk";
 import cors from "cors"
+import TaskModel from "./models/TaskModel.js";
 
 try {
   await connection_db.authenticate();
   console.log(chalk(`✅ Connection has been established successfully.\n`));
 } catch (error) {
   console.error('❌ Unable to connect to the database:', error);
+}
+
+try {
+  await TaskModel.sync(); //await TaskModel.sync({force: true}); // Elimina el modelo anterior y crea uno nuevo
+  console.log(chalk('✅ Model and database synchronized correctly\n'));
+} catch (error) {
+  console.error('❌ Synchronization error:', error);
 }
 
 export const app = express();
--- backend/models/TaskModel.js
+++ backend/models/TaskModel.js
@@ -1,6 +1,5 @@
 import { DataTypes } from 'sequelize';
 import connection_db from '../database/connection_db.js';
-import chalk from 'chalk';
 
 const TaskModel = connection_db.define('Task', {
     id: {
@@ -9,19 +8,12 @@ const TaskModel = connection_db.define('Task', {
         primaryKey: true
     },
     name: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(50),
         allowNull: false
     }
 }, { // Opciones adicionales del modelo
     tableName: 'tasks', // Nombre de la tabla en la base de datos
     timestamps: false // Deshabilitar los campos createdAt y updatedAt
 });
-
-try {
-    await TaskModel.sync(); //await TaskModel.sync({force: true}); // Elimina el modelo anterior y crea uno nuevo
-    console.log(chalk('✅ Model and database synchronized correctly\n'));
-  } catch (error) {
-    console.error('❌ Synchronization error:', error);
-  }
 
 export default TaskModel;
